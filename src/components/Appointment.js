0113ca5e4f0975b1eb229927a5d32d50f8414913
Alvaro Condori <condorialvaro95@gmail.com>
2025-02-06 21:43:08 -0300

    finished project

---
+++ src/components/Appointment.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Appointment = () => {
+  return (
+    <div className='bg-accent w-full p-8 rounded-sm max-w-[22rem] md:max-w-md mx-auto'>
+      <h3 className='text-white text-[28px] font-bold tracking-[0.3px] text-center'>
+        Get an appointment
+      </h3>
+      <div className='w-[54px] h-[3px] bg-white my-6 mx-auto'></div>
+      {/* input group */}
+      <div className='space-y-[24px]'>
+        <input type='text' className='form-control' placeholder='Full name' />
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Phone number'
+        />
+        <input
+          type='email'
+          className='form-control'
+          placeholder='Email address'
+        />
+        <textarea
+          className='resize-none w-full h-[132px] outline-none rounded-sm p-4 font-body text-sm text-gray focus:ring-1 focus:ring-primary'
+          placeholder='Your message'></textarea>
+        <button className='btn bg-primary hover:bg-primary-hover transition-all'>
+          Send message
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Appointment;
